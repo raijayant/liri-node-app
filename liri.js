93a328084de187950771a038ae9a41c49b7b35a0
jayantrai <jayant.chamling@outlook.com>
2019-04-25 18:04:36 -0700

    commit

--- liri.js
+++ liri.js
@@ -1,5 +1,9 @@
+// it is always a good idea to set up environment variables to protect your tokens, passwords, db connection strings, etc. It is also great for having one reference to a variable that may change.
 require("dotenv").config();
 
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 
+
+var keys = require("./keys.js");
+var spotify = require("spotify");
+
+console.log("load");
